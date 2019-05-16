d48807953bd6d7a8be9f21cbb610cd4782f2cb22
Marcelo Aguero <marceloaguero@gmail.com>
2019-05-16 19:56:55 -0300

    Fix indentation

--- webServer_test.go
+++ webServer_test.go
@@ -19,9 +19,9 @@ func createTable() {
 
 	const query = `
 		CREATE TABLE IF NOT EXISTS users (
-		  id SERIAL PRIMARY KEY,
-		  first_name TEXT,
-		  last_name TEXT
+			id SERIAL PRIMARY KEY,
+			first_name TEXT,
+			last_name TEXT
 	)`
 
 	_, err = db.Exec(query)
